ce05d47569bd3a9c500b1020f447a801092a7cdd
rhmt <rezaeihazara@gmail.com>
2019-07-13 19:54:41 +0900

    feat: login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
   root 'static_pages#home'
 
   get  '/help', to: 'static_pages#help'
@@ -7,6 +8,11 @@ Rails.application.routes.draw do
   get  '/contact', to: 'static_pages#contact'
 
   get '/signup', to: 'users#new'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
 
   post '/signup',  to: 'users#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
