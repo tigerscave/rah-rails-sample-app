7ba73f958cfde871c51525b12b9dde06ba57a62a
rhmt <rezaeihazara@gmail.com>
2019-07-20 10:56:42 +0900

    login with remember user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
 
+
+
+  attr_accessor :remember_token
+
   before_save { email.downcase! }
 
   validates :name, presence: true, length: { maximum: 50}
@@ -11,4 +15,31 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }
 
   has_secure_password
+
+  def User.digest(string)
+
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    User.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    return false if remeber_digest.nil?
+    Bcrypt::Password.new(remember_digest).is_password?(remember_token)
+
+  end
+
+  def forget
+    update_attribute(:remeber_digest, nil)
+  end
+
 end
