62a1b2fcec487dc441abc83386598a507ab2516c
rhmt <rezaeihazara@gmail.com>
2019-07-21 13:23:43 +0900

    chore: fix update user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
 
     if @user.update_attributes(user_params)
-      # handle a success update
+      flash[:success] = "Profile updated"
+      redirect_to @user
     else
       render 'edit'
     end
