59b08c957f31800600832a0c3e2abeb20400d9f9
rhmt <rezaeihazara@gmail.com>
2019-07-23 16:08:22 +0900

    feat: account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
 
-  attr_accessor :remember_token
-  before_save { email.downcase! }
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true, length: { maximum: 50}
@@ -32,4 +33,14 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remeber_digest, nil)
   end
+
+  private
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,9 @@ User.create!(name: "example User",
              email: "example@railstutorial.org",
              password: "password",
              password_confirmation: 'password',
-             admin: true
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now
 )
 
 20.times do |n|
@@ -21,6 +23,8 @@ User.create!(name: "example User",
                email: email,
                password: password,
                password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now
                )
 end
 
