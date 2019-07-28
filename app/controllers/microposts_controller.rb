6d2f1c02ee2cd782c350600b9a87a8b35a7f77bc
rhmt <rezaeihazara@gmail.com>
2019-07-28 22:03:23 +0900

    feat: destroy micropost

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,7 @@
 class MicropostsController < ApplicationController
+
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user, only: :destroy #BUG this method is not working ...
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -7,16 +9,26 @@ class MicropostsController < ApplicationController
       flash[:success] = "Micropost created!"
       redirect_to root_url
     else
+      @feed_items = []
       render 'static_pages/home'
     end
   end
 
   def destroy
+    correct_user
+    @micropost.destroy
+    flash[:success] = "Micropost deleted"
+    redirect_to request.referrer || root_url
   end
 
   private
-
     def micropost_params
       params.require(:micropost).permit(:content)
     end
+
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
+    end
+
 end
